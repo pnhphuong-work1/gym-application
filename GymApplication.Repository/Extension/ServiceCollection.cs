2f48303f44bc8d53981405c318bacb10b6f71a75
lqviet45 <lqviet455@gmail.com>
2024-09-24 19:39:37 +0700

    adding identity

--- GymApplication.Repository/Extension/ServiceCollection.cs
+++ GymApplication.Repository/Extension/ServiceCollection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GymApplication.Repository.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,7 +11,8 @@ public static class ServiceCollection
     public static IServiceCollection AddRepository(this IServiceCollection collection, IConfiguration configuration)
     {
         return collection
-            .AddDatabase(configuration);
+            .AddDatabase(configuration)
+            .AddIdentity();
     }
     
     private static IServiceCollection AddDatabase(this IServiceCollection collection, IConfiguration configuration)
@@ -19,6 +22,32 @@ public static class ServiceCollection
             options
                 .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
                 .AddInterceptors();
+        });
+        
+        return collection;
+    }
+    
+    private static IServiceCollection AddIdentity(this IServiceCollection collection)
+    {
+        collection.AddIdentityCore<ApplicationUser>(opt =>
+            {
+                opt.Lockout.AllowedForNewUsers = true;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+            })
+            .AddRoles<ApplicationRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        collection.Configure<IdentityOptions>(op =>
+        {
+            op.Lockout.AllowedForNewUsers = true;
+            op.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+            op.Lockout.MaxFailedAccessAttempts = 5;
+            op.Password.RequiredLength = 6;
+            op.Password.RequireDigit = false;
+            op.Password.RequireLowercase = false;
+            op.Password.RequireUppercase = false;
+            op.Password.RequireNonAlphanumeric = false;
         });
         
         return collection;
