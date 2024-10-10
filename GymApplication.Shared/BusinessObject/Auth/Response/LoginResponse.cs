8197c5765975abc8a3d70d91e5b8da9c701103b4
lqviet45 <lqviet455@gmail.com>
2024-10-10 17:34:40 +0700

    adding reset password

--- GymApplication.Shared/BusinessObject/Auth/Response/LoginResponse.cs
+++ GymApplication.Shared/BusinessObject/Auth/Response/LoginResponse.cs
@@ -4,6 +4,8 @@ public sealed class LoginResponse
 {
     public string? AccessToken { get; init; }
     public string? RefreshToken { get; init; }
+    public string? Role { get; init; }
+    public string? FullName { get; init; }
     public DateTime? AccessTokenExpiration { get; init; }
     public DateTime? RefreshTokenExpiration { get; init; }
 }
