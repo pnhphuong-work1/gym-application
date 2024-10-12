e467a83d1df00d201a3be9b7fe32b51282efc6e6
phatnh13 <phatnguyenhoang69@gmail.com>
2024-10-12 14:17:56 +0700

    <add> subscription: get, getbyId, create

---
+++ GymApplication.Services/Feature/SubcriptionFeatures/CreateSubscriptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GymApplication.Repository.Entities;
+using GymApplication.Repository.Repository.Abstraction;
+using GymApplication.Services.Abstractions;
+using GymApplication.Shared.BusinessObject.Subscription.Request;
+using GymApplication.Shared.BusinessObject.Subscription.Respone;
+using GymApplication.Shared.BusinessObject.User.Request;
+using GymApplication.Shared.BusinessObject.User.Response;
+using GymApplication.Shared.Common;
+using MediatR;
+using UUIDNext;
+
+namespace GymApplication.Services.Feature.SubcriptionFeatures;
+
+public class CreateSubscriptionHandler : IRequestHandler<CreateSubscriptionRequest, Result<SubscriptionResponse>>
+{
+    private readonly IRepoBase<Subscription, Guid> _subscriptionRepository;
+    private readonly IMapper _mapper;
+    private readonly ICacheServices _cacheServices;
+
+    public CreateSubscriptionHandler(IRepoBase<Subscription, Guid> subscriptionRepository, IMapper mapper,
+        ICacheServices cacheServices)
+    {
+        _subscriptionRepository = subscriptionRepository;
+        _mapper = mapper;
+        _cacheServices = cacheServices;
+    }
+
+    public Task<Result<SubscriptionResponse>> Handle(CreateSubscriptionRequest request,
+        CancellationToken cancellationToken)
+    {
+        var subscription = new Subscription()
+        {
+            Id = Uuid.NewDatabaseFriendly(Database.PostgreSql),
+            Name = request.Name,
+            Price = request.Price,
+            TotalWorkoutTime = request.TotalWorkoutTime,
+            CreatedAt = DateTime.UtcNow
+        };
+        _subscriptionRepository.Add(subscription);
+
+        var response = _mapper.Map<SubscriptionResponse>(subscription);
+
+        _cacheServices.SetAsync(subscription.Id.ToString(), response, TimeSpan.FromMinutes(5), cancellationToken);
+
+        return Task.FromResult(Result.Success(response));
+    }
+}
---
+++ GymApplication.Shared/BusinessObject/Subscription/Request/CreateSubscriptionRequest.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using GymApplication.Shared.BusinessObject.Subscription.Respone;
+using GymApplication.Shared.Common;
+using MediatR;
+
+namespace GymApplication.Shared.BusinessObject.Subscription.Request;
+
+public sealed class CreateSubscriptionRequest : IRequest<Result<SubscriptionResponse>>
+{
+    public string Name { get; set; }
+    public TimeSpan TotalWorkoutTime { get; set; }
+    public decimal Price { get; set; }
+}
+public sealed class CreateSubscriptionRequestValidation : AbstractValidator<CreateSubscriptionRequest> {
+    public CreateSubscriptionRequestValidation()
+    {
+        RuleFor(s => s.Name)
+            .NotNull().NotEmpty();
+
+        RuleFor(s => s.Price)
+            .GreaterThan(0).NotNull();
+
+        RuleFor(s => s.TotalWorkoutTime)
+            .NotNull();
+    }
+}
