50a3e8de3e5aefb8b084b5606c8eca8a4d7ec18d
phatnh13 <phatnguyenhoang69@gmail.com>
2024-10-12 19:13:25 +0700

    <create> handler for daygroup delete and update

---
+++ GymApplication.Services/Feature/DayGroupFeature/DeleteDayGroupHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using GymApplication.Repository.Entities;
+using GymApplication.Repository.Repository.Abstraction;
+using GymApplication.Services.Abstractions;
+using GymApplication.Shared.BusinessObject.DayGroups.Request;
+using GymApplication.Shared.Common;
+using MediatR;
+
+namespace GymApplication.Services.Feature.DayGroupFeature;
+
+public sealed class DeleteDayGroupHandler : IRequestHandler<DeleteDayGroupRequest, Result>
+{
+    private readonly IRepoBase<DayGroup, Guid> _dayGroupRepository;
+    private readonly ICacheServices _cacheServices; 
+
+    public DeleteDayGroupHandler(IRepoBase<DayGroup, Guid> dayGroupRepository, ICacheServices cacheServices)
+    {
+        _dayGroupRepository = dayGroupRepository;
+        _cacheServices = cacheServices;
+    }
+
+    public async Task<Result> Handle(DeleteDayGroupRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var dayGroup =
+                await _dayGroupRepository.GetByIdAsync(request.Id, new Expression<Func<DayGroup, object>>[0]);
+
+            if (dayGroup is null)
+            {
+                var notFoundError = new Error("404", "DayGroup not found");
+
+                return Result.Failure(notFoundError);
+            }
+
+            _dayGroupRepository.Delete(dayGroup);
+
+            await _cacheServices.RemoveAsync(request.Id.ToString(), cancellationToken);
+            return Result.Success();
+        }
+        catch(Exception ex)
+        {
+            var error = new Error("500", "Failed to delete DayGroup");
+        
+            return Result.Failure(error);
+        }
+    }
+}
---
+++ GymApplication.Services/Feature/DayGroupFeature/UpdateDayGroupHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using GymApplication.Repository.Entities;
+using GymApplication.Repository.Repository.Abstraction;
+using GymApplication.Services.Abstractions;
+using GymApplication.Shared.BusinessObject.DayGroup.Request;
+using GymApplication.Shared.Common;
+using MediatR;
+
+namespace GymApplication.Services.Feature.DayGroupFeature;
+
+public sealed class UpdateDayGroupHandler : IRequestHandler<UpdateDayGroupRequest, Result>
+{
+    private readonly IRepoBase<DayGroup, Guid> _dayGroupRepository;
+    private readonly ICacheServices _cacheServices; 
+
+    public UpdateDayGroupHandler(IRepoBase<DayGroup, Guid> dayGroupRepoBase, ICacheServices cacheServices)
+    {
+        _dayGroupRepository = dayGroupRepoBase;
+        _cacheServices = cacheServices;
+    }
+
+    public async Task<Result> Handle(UpdateDayGroupRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var dayGroup = await _dayGroupRepository.GetByIdAsync(request.Id, new Expression<Func<DayGroup, object>>[0]);
+
+            if (dayGroup is null)
+            {
+                var notFoundError = new Error("DayGroup not found", "DayGroup not found");
+            
+                return Result.Failure(notFoundError);
+            }
+
+            dayGroup.Group = request.Group;
+            dayGroup.UpdatedAt = DateTime.Now;
+
+            _dayGroupRepository.Update(dayGroup);
+
+            await _cacheServices.SetAsync(dayGroup.Id.ToString(), dayGroup, cancellationToken);
+            return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            var error = new Error("Failed to update DayGroup", ex.Message);
+            return Result.Failure(error);
+        }
+    }
+}
