daf858f904687cf55fa08e517b43471861d1106e
lqviet45 <lqviet455@gmail.com>
2024-10-12 16:24:09 +0700

    commit

---
+++ GymApplication.Services/Feature/Revenue/GetRevenueRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using GymApplication.Repository.Repository.Abstraction;
+using GymApplication.Shared.BusinessObject.Revenue.Request;
+using GymApplication.Shared.BusinessObject.Revenue.Response;
+using GymApplication.Shared.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymApplication.Services.Feature.Revenue;
+
+public class GetRevenueRequestHandler : IRequestHandler<GetRevenueRequest, Result<RevenueResponse>>
+{
+    private readonly IPaymentLogRepository _paymentLogRepository;
+
+    public GetRevenueRequestHandler(IPaymentLogRepository paymentLogRepository)
+    {
+        _paymentLogRepository = paymentLogRepository;
+    }
+
+    public async Task<Result<RevenueResponse>> Handle(GetRevenueRequest request, CancellationToken cancellationToken)
+    {
+        var paymentLogs = await _paymentLogRepository.GetByConditionsAsync(x => x.CreatedAt.Month == request.Month && x.CreatedAt.Year == request.Year);
+}
---
+++ GymApplication.api/Controllers/RevenueController.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning;
+using GymApplication.api.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymApplication.api.Controllers;
+
+[ApiController]
+[ApiVersion("2024-09-29")]
+[Route("api/v{version:apiVersion}/revenues")]
+public class RevenueController : RestController
+{
+    private readonly ISender _sender;
+
+    public RevenueController(ISender sender)
+    {
+        _sender = sender;
+    }
+}
