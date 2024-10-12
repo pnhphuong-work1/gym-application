53e922828363e5c18e05bcdc121450fccc7a11b6
phatnh13 <phatnguyenhoang69@gmail.com>
2024-10-12 19:12:57 +0700

    <uppdate> DayGroup controller with delete and update

--- GymApplication.api/Controllers/DayGroupController.cs
+++ GymApplication.api/Controllers/DayGroupController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
 using GymApplication.api.Common;
+using GymApplication.Shared.BusinessObject.DayGroup.Request;
 using GymApplication.Shared.BusinessObject.DayGroups.Request;
 using GymApplication.Shared.BusinessObject.DayGroups.Respone;
 using GymApplication.Shared.Common;
@@ -45,12 +46,36 @@ public class DayGroupController : RestController
     [HttpPost]
     [ProducesResponseType(201, Type = typeof(Result<DayGroupResponse>))]
     [ProducesResponseType(400, Type = typeof(Result))]
-    public async Task<IResult> Create([FromBody] CreateDayGroupRequest request)
+    public async Task<IResult> Post([FromBody] CreateDayGroupRequest request)
     {
         var result = await _mediator.Send(request);
 
         return result.IsSuccess
             ? Results.CreatedAtRoute("GetDayGroupById", new { id = result.Value.Id }, result)
+            : HandlerFailure(result);
+    }
+    
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(200, Type = typeof(Result<DayGroupResponse>))]
+    [ProducesResponseType(404, Type = typeof(Result))]
+    public async Task<IResult> Put(Guid id, [FromBody] UpdateDayGroupRequest request)
+    {
+        request.Id = id;
+        var result = await _mediator.Send(request);
+        return result.IsSuccess 
+            ? Results.Ok(result) 
+            : HandlerFailure(result);
+    }
+    
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(200, Type = typeof(Result))]
+    [ProducesResponseType(404, Type = typeof(Result))]
+    public async Task<IResult> Delete([FromRoute] Guid id)
+    {
+        var request = new DeleteDayGroupRequest(id);
+        var result = await _mediator.Send(request);
+        return result.IsSuccess 
+            ? Results.Ok(result) 
             : HandlerFailure(result);
     }
 }
