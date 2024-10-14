81c4e2f1a43c2259f60534a773b353a088a8190c
Phan Nguyễn Hoàng Phương <pnhphuong.work1@gmail.com>
2024-10-14 15:49:57 +0700

    added: CRUD functions in SubscriptionUserController.cs current error: Mapping error UserSubscription -> SubscriptionUserResponse at GETALL function

---
+++ GymApplication.Shared/BusinessObject/SubscriptionUser/Request/GetSubscriptionUserByIdRequest.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GymApplication.Shared.BusinessObject.Subscription.Request;
+using GymApplication.Shared.BusinessObject.Subscription.Respone;
+using GymApplication.Shared.BusinessObject.SubscriptionUser.Response;
+using GymApplication.Shared.Common;
+using MediatR;
+
+namespace GymApplication.Shared.BusinessObject.SubscriptionUser.Request;
+
+public sealed record GetSubscriptionUserByIdRequest(Guid Id) : IRequest<Result<SubscriptionUserResponse>>;
+
+public sealed class GetSubscriptionUserByIdValidation : AbstractValidator<GetSubscriptionUserByIdRequest> {
+    public GetSubscriptionUserByIdValidation()
+    {
+        RuleFor(u => u.Id)
+            .NotEmpty();
+    }
+}
---
+++ GymApplication.Shared/BusinessObject/SubscriptionUser/Response/SubscriptionUserResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace GymApplication.Shared.BusinessObject.SubscriptionUser.Response;
+
+public sealed class SubscriptionUserResponse
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid SubscriptionId { get; set; }
+    public Guid PaymentId { get; set; }
+    public decimal PaymentPrice { get; set; }
+    public int WorkoutSteak { get; set; }
+    public int LongestWorkoutSteak { get; set; }
+    public DateTime LastWorkoutDate { get; set; }
+    public DateTime SubscriptionEndDate { get; set; }
+}
