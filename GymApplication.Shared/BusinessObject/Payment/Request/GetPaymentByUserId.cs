00ea4b5e76b5fdfd910b87a119910b0bc951fb6a
lqviet45 <lqviet455@gmail.com>
2024-10-14 22:15:11 +0700

    adding GetPaymentByUserId

---
+++ GymApplication.Shared/BusinessObject/Payment/Request/GetPaymentByUserId.cs
@@ -0,0 +1,18 @@
+﻿using GymApplication.Shared.BusinessObject.Payment.Response;
+using GymApplication.Shared.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymApplication.Shared.BusinessObject.Payment.Request;
+
+public sealed class GetPaymentByUserId : IRequest<Result<PagedResult<PaymentReturnResponse>>>
+{
+    [FromRoute(Name = "userId")]
+    public Guid UserId { get; set; }
+    
+    [FromQuery]
+    public int Page { get; set; }
+    
+    [FromQuery]
+    public int PageSize { get; set; }
+}
--- GymApplication.api/Controllers/PaymentController.cs
+++ GymApplication.api/Controllers/PaymentController.cs
@@ -48,6 +48,16 @@ public class PaymentController : RestController
             : HandlerFailure(result);
     }
     
+    [HttpGet("user/{userId}")]
+    public async Task<IResult> GetPaymentByUserId(GetPaymentByUserId request)
+    {
+        var result = await _sender.Send(request);
+        
+        return result.IsSuccess ? 
+            Results.Ok(result)
+            : HandlerFailure(result);
+    }
+    
     // [HttpGet("{id}")]
     // public async Task<IResult> GetPaymentById([FromRoute] Guid id)
     // {
