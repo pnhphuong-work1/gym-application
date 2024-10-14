88c6deeabcfe1d42224b3455f0aab2d58a40f9fe
lqviet45 <lqviet455@gmail.com>
2024-10-14 18:50:56 +0700

    fix bugs

--- GymApplication.Shared/BusinessObject/User/Response/UserResponse.cs
+++ GymApplication.Shared/BusinessObject/User/Response/UserResponse.cs
@@ -6,5 +6,6 @@ public sealed class UserResponse
     public string UserName { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
     public string PhoneNumber { get; set; } = string.Empty;
 }
